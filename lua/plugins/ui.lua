9075706a6a082bd0798f223c6dd51a886fc4ccbb
Andrew Lolotronop <lolotron300037200123@gmail.com>
2023-11-07 18:45:51 +0700

    remove telescope tag

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -137,7 +137,6 @@ return {
 
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.1',
         event = 'VeryLazy',
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
