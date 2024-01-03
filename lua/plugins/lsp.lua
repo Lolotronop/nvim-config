156b4131274d728481656bcc8dc6067ca409aaf2
Andrew Lolotronop <lolotron300037200123@gmail.com>
2024-01-03 19:07:31 +0700

    fix mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,6 +29,14 @@ return {
                 lsp.default_keymaps({ buffer = bufnr })
             end)
 
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                -- ensure_installed = {'tsserver', 'rust_analyzer'},
+                handlers = {
+                    lsp.default_setup,
+                },
+            })
+
             -- (Optional) Configure lua language server for neovim
             require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
