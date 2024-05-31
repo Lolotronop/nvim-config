69c44c2d93e16fc16e28ff043df6a48c3b221035
Andrew Lolotronop <lolotron300037200123@gmail.com>
2024-05-31 13:51:00 +0700

    add tailwind class sorter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -143,6 +143,27 @@ return {
         },
     },
 
+    {
+        "laytan/tailwind-sorter.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+        build = "cd formatter && npm ci && npm run build",
+        config = {
+            on_save_enabled = true,
+            on_save_pattern = {
+                "*.html",
+                "*.js",
+                "*.jsx",
+                "*.tsx",
+                "*.twig",
+                "*.hbs",
+                "*.php",
+                "*.heex",
+                "*.astro",
+                "*.svelte",
+            },
+        },
+    },
+
     { -- Autocompletion
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
