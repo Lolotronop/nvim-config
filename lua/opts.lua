f04f036675881406ca5030ae405f29e40f3b3e8f
Andrew Lolotronop <lolotron300037200123@gmail.com>
2025-01-22 00:00:13 +0700

    add virtual text for diagnoctics back in

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.diagnostic.config({ update_in_insert = true })
+vim.diagnostic.config({ update_in_insert = true, virtual_text = true })
 vim.opt.termguicolors = true
 
 vim.opt.expandtab = true
