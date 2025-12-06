2c92d351a841f96cb82bd1d097964a92f1789cdb
Andrew Lolotronop <lolotron300037200123@gmail.com>
2025-12-06 17:41:33 +0700

    migrate to snaks picker

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -29,72 +29,6 @@ return {
         end,
     },
 
-    { -- Fuzzy Finder (files, lsp, etc)
-        "nvim-telescope/telescope.nvim",
-        event = "VeryLazy",
-        branch = "0.1.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            { -- If encountering errors, see telescope-fzf-native README for install instructions
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-                cond = function()
-                    return vim.fn.executable("make") == 1
-                end,
-            },
-            { "nvim-telescope/telescope-ui-select.nvim" },
-            -- { "nvim-tree/nvim-web-devicons" },
-        },
-        config = function()
-            local actions = require("telescope.actions")
-            require("telescope").setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-j>"] = actions.move_selection_next,
-                            ["<C-k>"] = actions.move_selection_previous,
-                        },
-                    },
-                },
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown(),
-                    },
-                },
-            })
-
-            pcall(require("telescope").load_extension, "fzf")
-            pcall(require("telescope").load_extension, "ui-select")
-
-            -- See `:help telescope.builtin`
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-            vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-            vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-            vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-            vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-            vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-            vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-            vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-            vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
-            -- Also possible to pass additional configuration options.
-            --  See `:help telescope.builtin.live_grep()` for information about particular keys
-            vim.keymap.set("n", "<leader>s/", function()
-                builtin.live_grep({
-                    grep_open_files = true,
-                    prompt_title = "Live Grep in Open Files",
-                })
-            end, { desc = "[S]earch [/] in Open Files" })
-
-            -- Shortcut for searching your neovim configuration files
-            vim.keymap.set("n", "<leader>sn", function()
-                builtin.find_files({ cwd = vim.fn.stdpath("config") })
-            end, { desc = "[S]earch [N]eovim files" })
-        end,
-    },
-
     {
         "luisiacc/gruvbox-baby",
         lazy = false,
@@ -222,11 +156,28 @@ return {
                 quickfile = { enabled = true },
                 statuscolumn = { enabled = true },
                 words = { enabled = true },
+                picker = {
+                    enabled = true,
+                    layout = {
+                        width = 1,
+                    },
+                },
             }
             require("snacks").setup(opts)
             vim.keymap.set("n", "<leader>nh", function()
                 require("snacks").notifier.show_history()
             end, { desc = "Snack" })
+
+            vim.keymap.set("n", "<leader>sh", Snacks.picker.help, { desc = "[S]earch [H]elp" })
+            vim.keymap.set("n", "<leader>sk", Snacks.picker.keymaps, { desc = "[S]earch [K]eymaps" })
+            vim.keymap.set("n", "<leader>sf", Snacks.picker.files, { desc = "[S]earch [F]iles" })
+            -- vim.keymap.set("n", "<leader>ss", Snacks.picker., { desc = "[S]earch [S]elect Telescope" })
+            vim.keymap.set("n", "<leader>sw", Snacks.picker.grep_word, { desc = "[S]earch current [W]ord" })
+            vim.keymap.set("n", "<leader>sg", Snacks.picker.grep, { desc = "[S]earch by [G]rep" })
+            vim.keymap.set("n", "<leader>sd", Snacks.picker.diagnostics, { desc = "[S]earch [D]iagnostics" })
+            vim.keymap.set("n", "<leader>sr", Snacks.picker.resume, { desc = "[S]earch [R]esume" })
+            -- vim.keymap.set("n", "<leader>s.", Snacks.picker.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+            -- vim.keymap.set("n", "<leader><leader>", Snacks.picker.buffers, { desc = "[ ] Find existing buffers" })
         end,
     },
 
