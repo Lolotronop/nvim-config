6ff67033e406d39a497d5011a80553c5a8e571a1
Andrew Lolotronop <lolotron300037200123@gmail.com>
2026-03-31 19:55:15 +0700

    mini lazyloading

--- plugin/20_mini.lua
+++ plugin/20_mini.lua
@@ -5,107 +5,124 @@ vim.pack.add({
 
 require('mini.basics').setup()
 
-require("mini.diff").setup({
-    view = {
-        -- style = 'sign',
-        signs = { add = '+', change = '~', delete = '-' },
-    }
-})
-require("mini.git").setup()
+local mini_loaded = false
 
-require("mini.pairs").setup({
-    modes = { insert = true, command = true, terminal = false },
-    -- skip autopair when next character is one of these
-    skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-    -- skip autopair when the cursor is inside these treesitter nodes
-    skip_ts = { "string" },
-    -- skip autopair when next character is closing pair
-    -- and there are more closing pairs than opening pairs
-    skip_unbalanced = true,
-    -- better deal with markdown code blocks
-    markdown = true,
-})
+local function load_mini()
+    if mini_loaded then
+        return
+    end
+    mini_loaded = true
 
-local ai = require("mini.ai")
-ai.setup({
-    n_lines = 500,
-    custom_textobjects = {
-        o = ai.gen_spec.treesitter({ -- code block
-            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-        }),
-        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),       -- class
-        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },           -- tags
-    },
-})
+    require("mini.diff").setup({
+        view = {
+            -- style = 'sign',
+            signs = { add = '+', change = '~', delete = '-' },
+        }
+    })
+    require("mini.git").setup()
 
-require("mini.surround").setup({
-    mappings = {
-        add = "sa",
-        delete = "sd",
-        replace = "sr",
+    require("mini.pairs").setup({
+        modes = { insert = true, command = true, terminal = false },
+        -- skip autopair when next character is one of these
+        skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+        -- skip autopair when the cursor is inside these treesitter nodes
+        skip_ts = { "string" },
+        -- skip autopair when next character is closing pair
+        -- and there are more closing pairs than opening pairs
+        skip_unbalanced = true,
+        -- better deal with markdown code blocks
+        markdown = true,
+    })
 
-        find = "sf",           -- Find surrounding (to the right)
-        find_left = "sF",      -- Find surrounding (to the left)
-        highlight = "sh",      -- Highlight surrounding
-        update_n_lines = "sn", -- Update `n_lines`
+    local ai = require("mini.ai")
+    ai.setup({
+        n_lines = 500,
+        custom_textobjects = {
+            o = ai.gen_spec.treesitter({ -- code block
+                a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+            }),
+            f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+            c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),       -- class
+            t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },           -- tags
+        },
+    })
 
-        suffix_last = "l",     -- Suffix to search with "prev" method
-        suffix_next = "n",     -- Suffix to search with "next" method
-    },
-})
+    require("mini.surround").setup({
+        mappings = {
+            add = "sa",
+            delete = "sd",
+            replace = "sr",
 
-require("mini.icons").setup()
-MiniIcons.mock_nvim_web_devicons()
+            find = "sf",           -- Find surrounding (to the right)
+            find_left = "sF",      -- Find surrounding (to the left)
+            highlight = "sh",      -- Highlight surrounding
+            update_n_lines = "sn", -- Update `n_lines`
 
--- TODO: maybe set it up?
-require("mini.statusline").setup()
+            suffix_last = "l",     -- Suffix to search with "prev" method
+            suffix_next = "n",     -- Suffix to search with "next" method
+        },
+    })
+
+    require("mini.icons").setup()
+    MiniIcons.mock_nvim_web_devicons()
+
+    -- TODO: maybe set it up?
+    require("mini.statusline").setup()
 
 
-local miniclue = require('mini.clue')
-miniclue.setup({
-    triggers = {
-        -- Leader triggers
-        { mode = { 'n', 'x' }, keys = '<Leader>' },
+    local miniclue = require('mini.clue')
+    miniclue.setup({
+        triggers = {
+            -- Leader triggers
+            { mode = { 'n', 'x' }, keys = '<Leader>' },
 
-        -- `[` and `]` keys
-        { mode = 'n',          keys = '[' },
-        { mode = 'n',          keys = ']' },
+            -- `[` and `]` keys
+            { mode = 'n',          keys = '[' },
+            { mode = 'n',          keys = ']' },
 
-        -- Built-in completion
-        { mode = 'i',          keys = '<C-x>' },
+            -- Built-in completion
+            { mode = 'i',          keys = '<C-x>' },
 
-        -- `g` key
-        { mode = { 'n', 'x' }, keys = 'g' },
+            -- `g` key
+            { mode = { 'n', 'x' }, keys = 'g' },
 
-        -- Marks
-        { mode = { 'n', 'x' }, keys = "'" },
-        { mode = { 'n', 'x' }, keys = '`' },
+            -- Marks
+            { mode = { 'n', 'x' }, keys = "'" },
+            { mode = { 'n', 'x' }, keys = '`' },
 
-        -- Registers
-        { mode = { 'n', 'x' }, keys = '"' },
-        { mode = { 'i', 'c' }, keys = '<C-r>' },
+            -- Registers
+            { mode = { 'n', 'x' }, keys = '"' },
+            { mode = { 'i', 'c' }, keys = '<C-r>' },
 
-        -- Window commands
-        { mode = 'n',          keys = '<C-w>' },
+            -- Window commands
+            { mode = 'n',          keys = '<C-w>' },
 
-        -- `z` key
-        { mode = { 'n', 'x' }, keys = 'z' },
-    },
+            -- `z` key
+            { mode = { 'n', 'x' }, keys = 'z' },
+        },
 
-    clues = {
-        -- Enhance this by adding descriptions for <Leader> mapping groups
-        miniclue.gen_clues.square_brackets(),
-        miniclue.gen_clues.builtin_completion(),
-        miniclue.gen_clues.g(),
-        miniclue.gen_clues.marks(),
-        miniclue.gen_clues.registers(),
-        miniclue.gen_clues.windows(),
-        miniclue.gen_clues.z(),
-    },
+        clues = {
+            -- Enhance this by adding descriptions for <Leader> mapping groups
+            miniclue.gen_clues.square_brackets(),
+            miniclue.gen_clues.builtin_completion(),
+            miniclue.gen_clues.g(),
+            miniclue.gen_clues.marks(),
+            miniclue.gen_clues.registers(),
+            miniclue.gen_clues.windows(),
+            miniclue.gen_clues.z(),
+        },
 
-    window = {
-        delay = 500,
-    }
+        window = {
+            delay = 500,
+        }
+    })
+end
+
+vim.api.nvim_create_autocmd("UIEnter", {
+    group = vim.api.nvim_create_augroup("myconf_load_mini", { clear = true }),
+    once = true,
+    callback = function()
+        vim.schedule(load_mini)
+    end,
 })
