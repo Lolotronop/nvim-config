ebb999c3ca973bb8a7d486aebc10a2dce1e749c5
Andrew Lolotronop <lolotron300037200123@gmail.com>
2026-04-18 23:00:13 +0700

    enable UI2 and scrolloff

--- plugin/10_opts.lua
+++ plugin/10_opts.lua
@@ -3,7 +3,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.g.loaded_matchit = 1
+vim.o.scrolloff = 4
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -51,4 +51,28 @@ vim.api.nvim_create_autocmd("BufReadPost", {
             pcall(vim.api.nvim_win_set_cursor, 0, mark)
         end
     end,
+})
+
+require("vim._core.ui2").enable({
+    enable = true, -- Whether to enable or disable the UI.
+    msg = { -- Options related to the message module.
+        ---@type 'cmd'|'msg' Default message target, either in the
+        ---cmdline or in a separate ephemeral message window.
+        ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+        ---or table mapping |ui-messages| kinds and triggers to a target.
+        targets = "cmd",
+        cmd = { -- Options related to messages in the cmdline window.
+            height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
+        },
+        dialog = { -- Options related to dialog window.
+            height = 0.5, -- Maximum height.
+        },
+        msg = { -- Options related to msg window.
+            height = 0.5, -- Maximum height.
+            timeout = 4000, -- Time a message is visible in the message window.
+        },
+        pager = { -- Options related to message window.
+            height = 1, -- Maximum height.
+        },
+    },
 })
