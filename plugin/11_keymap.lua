5e178b9640a260a8fd1fc78dbf9ab2a8b3ae14a1
Andrew Lolotronop <lolotron300037200123@gmail.com>
2026-06-26 22:07:54 +0700

    wrap quickfixlist motions

--- plugin/11_keymap.lua
+++ plugin/11_keymap.lua
@@ -14,14 +14,14 @@ vim.keymap.set("v", "<leader>r", 'y:%s/\\V<C-r>"//g<Left><Left>', {
     desc = "Replace visual selection in whole file",
 })
 
-local function qf_jump(command)
-    local size = vim.fn.getqflist({ size = 1 }).size
-    if size == 0 then return end
-    vim.cmd(size == 1 and "cc" or command)
+local function qf_jump(cmd, wrap)
+    local n = vim.fn.getqflist({ size = 1 }).size
+    if n == 0 then return end
+    if not pcall(vim.cmd, n == 1 and "cc" or cmd) then vim.cmd(wrap) end
 end
 
-vim.keymap.set("n", "<M-l>", function() qf_jump("cnext") end, { desc = "Next [L]ist" })
-vim.keymap.set("n", "<M-h>", function() qf_jump("cprevious") end, { desc = "Previous [L]ist" })
+vim.keymap.set("n", "<M-l>", function() qf_jump("cnext", "cfirst") end, { desc = "Next [L]ist" })
+vim.keymap.set("n", "<M-h>", function() qf_jump("cprevious", "clast") end, { desc = "Previous [L]ist" })
 
 vim.keymap.set("n", "<leader>rr", function()
     local buf = vim.api.nvim_get_current_buf()
